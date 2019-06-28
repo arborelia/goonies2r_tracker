9d731ae1c0ba15272a5d43eade62ac6cc0c6e673
arborelia <robyn.l.speer@gmail.com>
2019-06-27 20:30:47 -0400

    implemented like half of the rooms

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,8 +1,8 @@
 --  Load configuration options up front
 ScriptHost:LoadScript("scripts/settings.lua")
 
+Tracker:AddMaps("maps/maps.json")
 Tracker:AddItems("items/common.json")
-Tracker:AddLocations("locations/rooms.json")
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/tracker_broadcast.json")
-Tracker:AddMaps("maps/maps.json")
+Tracker:AddLocations("locations/rooms.json")
